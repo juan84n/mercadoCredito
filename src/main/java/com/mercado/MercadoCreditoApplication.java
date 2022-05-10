0887466309a2481b793110f606bd0d26997c62a5
Juan Felipe Narvaez <juanfelipe.narvaez@halopowered.com>
2022-05-10 15:08:17 -0500

    proyecto de mercado creditio

---
+++ src/main/java/com/mercado/MercadoCreditoApplication.java
@@ -0,0 +1,13 @@
+package com.mercado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoCreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoCreditoApplication.class, args);
+	}
+
+}
